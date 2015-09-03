e844c9cab78c4000475826c86e73a47e48ffdf8b
minling <mzhao725@gmail.com>
2015-09-03 15:55:53 -0400

    did some refactoring

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,9 @@ class SearchController < ApplicationController
   def index
     @keyword = params[:keyword]
     @word = Word.find_word(@keyword)
-    if Word.find_word(@keyword) != nil
+    if @word
       redirect_to "/words/#{@word.id}"
-    elsif Word.find_word(@keyword) == nil
+    else
       @fuzzy_words = Search.for(@keyword)
     end
   end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,8 +1,9 @@
 class Search
 
   def self.for(word)
-    words = Word.arel_table
-    fuzzy_words = Word.where(words[:name].matches("%#{word}%"))
+    # words = Word.arel_table
+    # fuzzy_words = Word.where(words[:name].matches("%#{word}%"))
+    Word.where("name LIKE  ?", "%#{word}%")
   end
 
 end
